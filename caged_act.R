d69e019e7ed8a514745c90fac74530bfcdedd85c
glaubereduardo <35948015+glaubereduardo@users.noreply.github.com>
2022-03-16 08:26:26 -0300

    Ajustes no processo de consolidação de novos dados por meio do salvamento de arquivo intermediário processado com registro da competência de declaração para evitar problemas com dupla consolidação de novos dados.

--- caged_act.R
+++ caged_act.R
@@ -8,8 +8,7 @@
 # Fazer download dos arquivos do CAGED disponíveis em ftp.mtps.gov.br/pdet/microdados/NOVO CAGED
 # Descompactar arquivos em uma pasta
 # Informar caminho da pasta
-# pasta.txt <- "M:/Dados/CAGED/" # Exemplo do meu computador
-pasta.txt <- "C:/R/caged/" # substituir pelo caminho da pasta no computador (manter a barra no final do caminho)
+pasta.txt <- "M:/Dados/CAGED/" # substituir pelo caminho da pasta no computador (manter a barra no final do caminho). Exemplo do meu computador
 
 # Bibliotecas
 library(data.table)
@@ -21,19 +20,13 @@ library(lubridate) # para parse datas
 # Fonte: https://www.ipea.gov.br/extrator/arquivos/160204_caracterizacao_br_re.pdf
 act_lista <- fread("acts_ipea.csv")
 
-# Divisão territorial brasileira
-# Fonte: https://www.ibge.gov.br/geociencias/organizacao-do-territorio/estrutura-territorial/23701-divisao-territorial-brasileira.html
-dtb <- fread("dtb_2021.csv")
-
-uf_regiao <- fread("uf_regiao.csv") # Nomes de UF e regiões brasileiras
-
 files <- list.files(path = pasta.txt, pattern="CAGED.*.txt") # Listar arquivos
 
 caged_file <- list() # Criar lista vazia para receber dados
 
 # Ler e selecionar dados dos múltiplos arquivos
 for (i in seq_along(files)) {
-  caged_file[[i]] <- fread(file = paste0(pasta.txt, files[i]), encoding = "UTF-8", select = c("competênciamov", "município", "subclasse", "saldomovimentação")) # Ler dados de cada arquivo
+  caged_file[[i]] <- fread(file = paste0(pasta.txt, files[i]), encoding = "UTF-8", select = c("competênciamov", "município", "subclasse", "saldomovimentação", "competênciadec")) # Ler dados de cada arquivo
   caged_file[[i]] <- caged_file[[i]][caged_file[[i]]$subclasse %in% act_lista$subclasse] # Selecionar apenas linhas de ACTs
   print(paste0("Arquivos processados: ",i)) # Acompanhar processamento
 }
@@ -45,7 +38,33 @@ caged <- clean_names(caged) # Limpar nomes das variáveis (bancos originais têm
 
 caged <- left_join(caged, act_lista[, .(subclasse, act)], by="subclasse") # Identificar ACT
 
-caged <- caged[, .(saldo = sum(saldomovimentacao)), by = .(competenciamov, municipio, act)] # Resumir saldo de empregos por mês, município e ACT
+caged <- caged[, .(saldo = sum(saldomovimentacao)), by = .(competenciamov, competenciadec, municipio, act)] # Resumir saldo de empregos por competências de declaração e movimentação, município e ACT
+
+caged_act_processed <- read_rds("caged_act_processed.rds") # Abre arquivo de registros anteriores processados
+
+caged <- rbind(caged_act_processed, caged) # Empilha novos registros
+
+caged <- distinct(caged) # Deleta registros repetidos (em caso de leitura repetida de dados brutos)
+
+write_rds(caged, "caged_act_processed.rds")
+
+
+
+
+################################################################################
+# Consolida registros previamente processados
+
+rm(list=ls()) # Deleta todos objetos do sistema
+
+# Divisão territorial brasileira
+# Fonte: https://www.ibge.gov.br/geociencias/organizacao-do-territorio/estrutura-territorial/23701-divisao-territorial-brasileira.html
+dtb <- fread("dtb_2021.csv")
+
+uf_regiao <- fread("uf_regiao.csv") # Nomes de UF e regiões brasileiras
+
+caged <- read_rds("caged_act_processed.rds") # Lê registros processados
+
+caged <- caged[, .(saldo = sum(saldo)), by = .(competenciamov, municipio, act)] # Resumir saldo de empregos por mês, município e ACT, utilizando registros anteriores e novos 
 
 caged$uf_codigo <- floor(caged$municipio/10000) # Criar variável UF
 
@@ -59,14 +78,5 @@ caged <- left_join(caged, uf_regiao, by = "uf_codigo") # Identificar nome da UF 
 caged$mes <- as.IDate(ym(caged$competenciamov)) # Transformar competenciamov para o formato data
 
 caged <- caged[, -c("competenciamov", "municipio", "uf_codigo")] # Excluir colunas desnecessárias
-
-
-caged_registosanteriores <- fread("caged_act.csv", encoding = "UTF-8") # Abre arquivo de registros anteriores
-caged_registosanteriores <- caged_registosanteriores[, -1] # Deleta coluna de ID
-
-caged <- rbind(caged_registosanteriores, caged, use.names=FALSE) # Empilha novos registros
-
-caged <- caged[, .(saldo = sum(saldo)), by = .(mes, Nome_Município, act)] # Resumir saldo de empregos por mês, município e ACT, utilizando registros anteriores e novos 
-
 
 write.csv(caged, "caged_act.csv", fileEncoding = "UTF-8") # Salvar arquivo csv
